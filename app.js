7f1183d4f7d301d5d6d3460355cf0eaef199cce6
Lerian Febriana <kanglerian@gmail.com>
2025-04-08 16:56:00 +0700

    add dan show

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
+const expressLayouts = require('express-ejs-layouts');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+
+const { Site } = require('./models');
 
 const indexRouter = require('./routes/index');
 const sitesRouter = require('./routes/sites');
@@ -18,12 +21,29 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cookieParser());
+app.use(expressLayouts);
 app.use(session({ secret: 'kanglerian' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/sites', sitesRouter);
+
+app.get('/in/:target', async (req, res) => {
+  try {
+    const site = await Site.findOne({
+      where: {
+        target: req.params.target,
+      }
+    });
+    if(!site) {
+      return res.status(404).send('Site not found');
+    }
+    return res.redirect(302, site.source);
+  } catch (error) {
+    return res.send(error.message);
+  }
+});
 
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/sites.js
+++ routes/sites.js
@@ -8,11 +8,34 @@ router.get('/', authCookie.check, async function(req, res, next) {
     const sites = await Site.findAll();
     return res.render('sites', {
       layout: 'template',
-      sites: sites
+      sites: sites,
+      url: req.protocol + '://' + req.get('host'),
     });
   } catch (error) {
     return res.send(error.message);
   }
+});
+
+router.post('/', authCookie.check, async function(req, res, next) {
+  try {
+    const { name, target, source } = req.body;
+    if (!name || !target || !source) {
+      return res.status(400).send('All fields are required');
+    }
+    await Site.create({
+      name: name,
+      target: target,
+      source: source,
+      status: true,
+    });
+    return res.redirect(302, '/sites');
+  } catch (error) {
+    return res.send(error.message);
+  }
+});
+
+router.delete('/:id', authCookie.check, async function(req, res, next){
+  return res.send(req.params.id);
 });
 
 module.exports = router;
